155649b0db95414afd152b3ae676ddd3772d83a6
janvi-kalra <kalrajanvi99@gmail.com>
2023-04-20 19:11:10 -0700

    changed to v3!

--- intercept.js
+++ intercept.js
@@ -1,24 +1,19 @@
-(function () {
-  var xhr = {};
-  xhr.open = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function (method, url) {
-    if (url.indexOf("/?o=") != -1) {
-      this.addEventListener("load", function () {
-        window.postMessage(
-          {
-            detail: this.responseText,
-            secret: "somerandompassword",
-            message: "NEW_SUBTITLE",
-          },
-          document.location.toString()
-        );
-        // console.log(
-        //   `calling intercept of subs with document.location ${document.location.toString()} and response text: ${
-        //     this.responseText
-        //   }`
-        // );
-      });
-    }
-    xhr.open.apply(this, arguments);
-  };
-})();
+// (function () {
+//   var xhr = {};
+//   xhr.open = XMLHttpRequest.prototype.open;
+//   XMLHttpRequest.prototype.open = function (method, url) {
+//     if (url.indexOf("/?o=") != -1) {
+//       this.addEventListener("load", function () {
+//         window.postMessage(
+//           {
+//             detail: this.responseText,
+//             secret: "somerandompassword",
+//             message: "NEW_SUBTITLE",
+//           },
+//           document.location.toString()
+//         );
+//       });
+//     }
+//     xhr.open.apply(this, arguments);
+//   };
+// })();
